ef91b8147afee454fa22498e64116d3c349ed782
anilmisirlioglu <misirlioglisamet@gmail.com>
2021-03-09 18:44:35 +0300

    feat: docusaurus config customized

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,105 +1,109 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+    title: 'Asena Docs',
+    tagline: 'Yeni nesil anket ve çekiliş botu.',
+    url: 'https://wiki.asena.xyz',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.svg',
+    organizationName: 'asena-team', // Usually your GitHub org/user name.
+    projectName: 'asena-wiki', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'Asena',
+            logo: {
+                alt: 'Asena Logo',
+                src: 'img/logo.png',
+            },
+            items: [
+                {
+                    to: 'docs/',
+                    activeBasePath: 'docs',
+                    label: 'Docs',
+                    position: 'left',
+                },
+                {
+                    to: 'blog',
+                    label: 'Blog',
+                    position: 'left'
+                },
+                {
+                    href: 'https://github.com/anilmisirlioglu/Asena',
+                    label: 'GitHub | Asena',
+                    position: 'right',
+                }
+            ],
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Style Guide',
+                            to: 'docs/',
+                        },
+                        {
+                            label: 'Second Doc',
+                            to: 'docs/doc2/',
+                        },
+                    ],
+                },
+                {
+                    title: 'Bağlantılar',
+                    items: [
+                        {
+                            label: 'Discord',
+                            href: 'https://dc.asena.xyz',
+                        },
+                        {
+                            label: 'Botu Davet Et',
+                            href: 'https://invite.asena.xyz',
+                        },
+                        {
+                            label: 'YouTube',
+                            href: 'https://www.youtube.com/channel/UCB_bh_HEJG-jp5b8RCRy9XA',
+                        },
+                    ],
+                },
+                {
+                    title: 'Daha Fazla',
+                    items: [
+                        {
+                            label: 'Hata Bildirin',
+                            href: 'https://issue.asena.xyz'
+                        },
+                        // {
+                        //     label: 'Blog',
+                        //     to: 'blog'
+                        // },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Asena. Built with Docusaurus.`,
         },
-      ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Style Guide',
-              to: 'docs/',
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/edit/master/website/',
+                },
+                // blog: {
+                //     showReadingTime: true,
+                //     // Please change this to your repo.
+                //     editUrl:
+                //         'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                // },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
             },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
