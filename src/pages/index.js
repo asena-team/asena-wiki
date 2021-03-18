4b039edaf7630823317234b283119bf90a36d7bc
Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>
2021-03-19 02:22:02 +0300

    chore: changed quick start URL
    Signed-off-by: Anıl Mısırlıoğlu <misirlioglisamet@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ function Home(){
                                 'button button--outline button--secondary button--lg',
                                 styles.getStarted,
                             )}
-                            to={useBaseUrl('docs/commands')}>
+                            to={useBaseUrl('docs/general/')}>
                             Hızlı Başlangıç
                         </Link>
                     </div>
